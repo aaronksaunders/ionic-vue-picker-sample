53015d3ac99f45490862809eb43a743af8540367
Aaron Saunders <aaronksaunders@gmail.com>
2021-03-27 18:41:06 -0400

    updated example

--- src/App.vue
+++ src/App.vue
@@ -1,182 +1,114 @@
 <template>
-  <div id="app">
-    <ion-app>
-      <ion-header translucent>
-        <ion-toolbar>
-          <ion-title>Ionic VueJS Picker Sample</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content fullscreen class="ion-padding">
-        <ion-picker-controller></ion-picker-controller>
-        <ion-button @click="openSimplePicker">Open Single Column Picker</ion-button>
-        <ion-button @click="openComplexPicker">Open Multi Column Picker</ion-button>
-        <ion-item>
-          <ion-label>
-            <pre> {{results}} </pre>
-          </ion-label>
-        </ion-item>
-      </ion-content>
-    </ion-app>
-  </div>
+  <ion-app class="ion-padding">
+    <ion-content>
+      <ion-button @click="openSimplePicker">SHOW PICKER</ion-button>
+      <div style="margin:10px">
+       <pre>{{JSON.stringify(pickerResult, null,2)}}</pre>
+      </div>
+    </ion-content>
+  </ion-app>
 </template>
 
-<style src='@ionic/core/css/core.css'></style>
-<style src='@ionic/core/css/ionic.bundle.css'></style>
-
-<script>
-/* eslint-disable no-alert, no-console */
-export default {
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { IonApp, pickerController, IonButton, IonContent } from "@ionic/vue";
+export default defineComponent({
   name: "App",
-  components: {},
-  data() {
-    return {
-      results: ""
-    };
+  components: {
+    IonApp,
+    IonContent,
+    IonButton,
   },
-  methods: {
-    async openSimplePicker() {
-      // get the picker controller this way for now
-      const pickerController = document.querySelector("ion-picker-controller");
-      await pickerController.componentOnReady();
+  setup() {
 
-      // set the arry for the column information, you can set the name of the column
-      // and the array of data that should be rendered in the column
-      let colOptions = [
-        { name: "animal", data: ["Dog", "Cat", "Bird", "Lizard", "Chinchilla"] }
-      ];
-
-      // now create the picker, but first you need to create the columns
-      // in the proper format for ionic vue to deal with them
-      const picker = await pickerController.create({
-        columns: this.getColumns(1, colOptions),
-        buttons: [
-          {
-            text: "Cancel",
-            role: "cancel"
-          },
-          {
-            text: "Confirm",
-            role: "confirm",
-            handler: value => {
-              console.log("Got Value", value);
-              this.results = value;
-              picker.dismiss(value, "confirm");
-            }
-          }
-        ]
-      });
-
-      picker.onDidDismiss().then(v => {
-        console.log(v);
-      });
-
-      // present the picker
-      await picker.present();
-    },
-
-    /**
-     * this is a multi colmn picker
-     */
-    async openComplexPicker() {
-      // get the picker controller this way for now
-      const pickerController = document.querySelector("ion-picker-controller");
-      await pickerController.componentOnReady();
-
-      // set the array for the column information, you can set the name of the column
-      // and the array of data that should be rendered in the column. In the array of data
-      // to add to the column you can set the text that will be rendered and the value
-      // to be associated with the text when selected
-      //
-      // in this example, ["Monday", "Mon"], the picker will render the full text "Monday",
-      // but the value selected will be "Mon". If there is not a value included, then it
-      // will default to the index of the row
-      //
-      let colOptions = [
-        {
-          name: "day",
-          data: [
-            ["Monday", "Mon"],
-            ["Tuesday", "Tues"],
-            ["Wednesday", "Wed"],
-            ["Thursday", "Thurs"],
-            ["Friday", "Fri"]
-          ]
-        },
-        {
-          name: "time",
-          data: ["9a", "12p", "3p", "6p"]
-        }
-      ];
-
-      const picker = await pickerController.create({
-        columns: this.getColumns(2, colOptions),
-        buttons: [
-          {
-            text: "Cancel",
-            role: "cancel"
-          },
-          {
-            text: "Confirm",
-            handler: value => {
-              console.log("Got Value", value);
-              this.results = value;
-            }
-          }
-        ]
-      });
-
-      await picker.present();
-    },
-
+    const pickerResult = ref<any>();
+    
     // HELPER FUNCTIONS FOR PICKERS
-    getColumns(numColumns, columnOptions) {
-      let columns = [];
-      for (let i = 0; i < numColumns; i++) {
-        let len = columnOptions[i].data.length;
 
-        // if a na,e is provided then set the object to the
-        // the name provided
-        let colName = columnOptions[i].name || `col-${i}`;
-        let col = {
-          name: colName,
-          options: this.getColumnOptions(i, len, columnOptions)
-        };
-        columns.push(col);
-      }
-
-      return columns;
-    },
-    getColumnOptions(columnIndex, numOptions, columnOptions) {
-      let options = [];
+    const getColumnOptions = (
+      columnIndex: any,
+      numOptions: any,
+      columnOptions: any
+    ) => {
+      const options = [];
       for (let i = 0; i < numOptions; i++) {
         // if there is no value property provided in the column data
         // then set value to the row index
         if (typeof columnOptions[columnIndex].data[i] == "object") {
           options.push({
             text: columnOptions[columnIndex].data[i % numOptions][0],
-            value: columnOptions[columnIndex].data[i % numOptions][1]
+            value: columnOptions[columnIndex].data[i % numOptions][1],
           });
         } else {
           options.push({
             text: columnOptions[columnIndex].data[i % numOptions],
-            value: i
+            value: i,
           });
         }
       }
-
       return options;
-    }
-  }
-};
+    };
+
+    const getColumns = (numColumns: any, columnOptions: any) => {
+      const columns = [];
+      for (let i = 0; i < numColumns; i++) {
+        const len = columnOptions[i].data.length;
+        // if a na,e is provided then set the object to the
+        // the name provided
+        const colName = columnOptions[i].name || `col-${i}`;
+        const col = {
+          name: colName,
+          options: getColumnOptions(i, len, columnOptions),
+        };
+        columns.push(col);
+      }
+      return columns;
+    };
+
+    const openSimplePicker = async () => {
+      // set the arry for the column information, you can set the name of the column
+      // and the array of data that should be rendered in the column
+      const colOptions = [
+        {
+          name: "animal",
+          data: ["Dog", "Cat", "Bird", "Lizard", "Chinchilla"],
+        },
+      ];
+      // now create the picker, but first you need to create the columns
+      // in the proper format for ionic vue to deal with them
+      const picker = await pickerController.create({
+        columns: getColumns(1, colOptions),
+        buttons: [
+          {
+            text: "Cancel",
+            role: "cancel",
+          },
+          {
+            text: "Confirm",
+            role: "confirm",
+            handler: (value) => {
+              console.log("Got Value", value);
+              console.log(value);
+              picker.dismiss(value, "confirm");
+            },
+          },
+        ],
+      });
+      picker.onDidDismiss().then((v) => {
+        console.log("onDidDismiss", v);
+        pickerResult.value = v;
+      });
+      // present the picker
+      await picker.present();
+    };
+
+    return {
+      openSimplePicker,
+      pickerResult
+    };
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-import Ionic from "@ionic/vue";
-
-Vue.config.productionTip = false;
-Vue.use(Ionic);
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
